b92074bbd99f6973391f71a1ff7da3eb057251ba
IraSym <30781779+IraSym@users.noreply.github.com>
2019-06-05 12:25:15 +0300

    Add files via upload

---
+++ 1-28.05.py
@@ -0,0 +1,8 @@
+name=input("Whats your name?\n")
+age=input("How old are you?\n")
+town=input("Where do you live?\n")
+print("Hello, " + name +"!")
+print("Your age is " + age)
+print("You live in " + town)
+
+
