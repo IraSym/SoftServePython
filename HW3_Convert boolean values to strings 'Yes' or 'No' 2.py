cc073b9233b4597b683abec984e099c49551d107
IraSym <30781779+IraSym@users.noreply.github.com>
2019-07-03 10:15:35 +0300

    Add files via upload

---
+++ HW3_Convert boolean values to strings 'Yes' or 'No' 2.py
@@ -0,0 +1,11 @@
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_str (boo):
+    print(boo)
+    if boo == True:
+        print("Yes")
+    else:
+        print("No")    
+
+bool_str(5>0)
+bool_str(5<0)
